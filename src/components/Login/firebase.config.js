e11d6fb15d6cfac016d9c31ddca5384bb0c8f613
Riyad0821 <mdriyad0821@gmail.com>
2020-09-22 18:54:21 +0600

    get simple google login auth set

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAr7jkqV4HcPL0Kn-u1-acRYxQaJ9OYajo",
+    authDomain: "burj-al-arab-8bc39.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-8bc39.firebaseio.com",
+    projectId: "burj-al-arab-8bc39",
+    storageBucket: "burj-al-arab-8bc39.appspot.com",
+    messagingSenderId: "360847713966",
+    appId: "1:360847713966:web:a8dd5027502e295bcbef3f"
+  };
+  export default firebaseConfig;
