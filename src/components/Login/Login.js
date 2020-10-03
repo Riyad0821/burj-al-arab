9ca3e3c1ad5387e6061bfe0e47dd9f3df824a7cc
Riyad0821 <mdriyad0821@gmail.com>
2020-10-03 23:00:17 +0600

    Create booking field and front end setup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -23,14 +23,8 @@ const Login = () => {
             history.replace(from);
             // ...
           }).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
             var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
+            console.log(errorMessage);
           });
     }
     return (
